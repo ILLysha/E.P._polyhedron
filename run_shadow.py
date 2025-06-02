2d565ab1666e8d24edc6242c9b74b3120d1f06b8
ilysha Chernov <ilychernov24032006@mail.ru>
2025-06-02 13:42:14 +0300

    ready to check

--- run_shadow.py
+++ run_shadow.py
@@ -15,7 +15,7 @@ try:
         polyedr.draw(tk)
         delta_time = time() - start_time
         print(f"Изображение полиэдра '{name}' заняло {delta_time} сек.")
-        print("Искомая характерстика - ", polyedr.modification())
+        print("Искомая характерстика - ", (polyedr.modification()/Polyedr.H))
         input("Hit 'Return' to continue -> ")
 except (EOFError, KeyboardInterrupt):
     print("\nStop")
